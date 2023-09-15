4683e94dc0ddbed1345bc15ac36487828d02000e
Julia Glinka <julia.w.glinka@gmail.com>
2023-09-16 00:53:51 +0200

    Get 'href'attribute from clicked link

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 'use strict';
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+    //console.log(event);
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-    console.log(activeLink.classList.contains('active'));
-    }
+        activeLink.classList.remove('active');
+        //console.log(activeLink.classList.contains('active'));
+        }
 
     /* add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus): ' + clickedElement);
-
+    //console.log('clickedElement:', clickedElement);
+    //console.log('clickedElement (with plus): ' + clickedElement);
     clickedElement.classList.add('active');
-    console.log(clickedElement.classList.contains('active'));
+    //console.log(clickedElement.classList.contains('active'));
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article');
-
     for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-    console.log(activeArticle.classList.contains('active'));
-    }
+        activeArticle.classList.remove('active');
+        //console.log(activeArticle.classList.contains('active'));
+        }
+
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector('articleSelector');
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
-
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
